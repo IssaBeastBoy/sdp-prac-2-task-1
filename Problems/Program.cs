69299111a8bdf233c57fb70a4cd6b32172733588
Thulani Tshabalala <G16T5351@ict.ru.ac.za>
2019-02-21 15:40:28 +0200

    Adding the modifications plus the Problem.test

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,21 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        static string Prefix (string input)
+        {
+            string[] output=input.Split(" ");
+            return($"{input.Length},{output.Length}:{input}");
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Write the word you want info about...");
+            string input= Console.ReadLine();
+            Console.WriteLine(Prefix(input));
+            Console.ReadLine();
         }
     }
 }
