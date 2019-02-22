a79d942a834f3bf22c75edfee5509d77bf619404
Thulani Tshabalala <G16T5351@ict.ru.ac.za>
2019-02-22 10:17:23 +0200

    Update of unit test for program

--- Problems.test/UnitTest1.cs
+++ Problems.test/UnitTest1.cs
@@ -1,8 +1,17 @@
 using NUnit.Framework;
 using Problems;
+using System.Collections;
 
 namespace Tests
 {
+    public class MyTests
+{
+    [TestCaseSource(typeof(Tests), "TestCases")]
+    public string DivideTest(string d)
+    {
+        return Program.Prefix(d);
+    }
+}
     public class Tests
     {
         [SetUp]
@@ -10,7 +19,19 @@ namespace Tests
         {
 
         }
-
+        public static IEnumerable TestCases
+    {
+        get
+        {
+            yield return new TestCaseData("hello").Returns("5,1:hello");
+            yield return new TestCaseData("").Returns("0,0:");
+            yield return new TestCaseData("what    ...  did you say?? ").Returns("27,5:what    ...  did you say?? ");
+            yield return new TestCaseData(null).Returns(null);
+            yield return new TestCaseData("Boy bye").Returns("7,2:Boy bye");
+            yield return new TestCaseData("          ").Returns("10,0:          ");
+        }
+    }  
+/*
         [Test]
         public void Test1()
         {
@@ -34,5 +55,6 @@ namespace Tests
             string result =Program.Prefix("what    ...  did you say?? ");
             Assert.AreEqual("27,5:what    ...  did you say?? ", result);
         }
+        */
     }
 }
